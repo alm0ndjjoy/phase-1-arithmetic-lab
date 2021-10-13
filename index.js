bec9a69e77e4b9ce6d9419e94d2a30edc0242f1f
Joyce Huang <65410999+alm0ndjjoy@users.noreply.github.com>
2021-10-12 21:22:20 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random()*1) +1;
+
+let num3 = 4
+let num4 = 16
+let mod = num4 / num3;
+
+let max = Math.max(15,16,17,18,19,20);
