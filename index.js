e96bb93ff16320d34f95cb764edaba7249f1e1c2
Joyce Huang <65410999+alm0ndjjoy@users.noreply.github.com>
2021-11-01 18:30:32 -0400

    commit tree f292c2b0a6781d4b4807eb161e253b0db7110319

--- index.js
+++ index.js
@@ -2,10 +2,25 @@ let num1 = 31;
 let num2 = 2;
 let multiply = num1 * num2;
 
-let random = Math.floor(Math.random()*1) +1;
 
-let num3 = 4
-let num4 = 16
-let mod = num4 / num3;
+let random = Math.floor(Math.random(1) +1);
 
-let max = Math.max(15,16,17,18,19,20);
+let num3 = 104;
+let num4 = 25;
+let mod = num3%num4;
+
+
+let max = Math.max(19,20);
+
+
+
+
+
+
+
+
+
+
+
+
+
